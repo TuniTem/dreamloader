277e994b173957cf4044057c0b7146458f2e1737
quis345 <mr.smiley.face515@gmail.com>
2026-03-27 15:20:21 -0700

    added backend and hooked it up

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,13 @@
 extends Control
 
 
-const DEFAULT_TITLE_FORMAT = "[title] - [author] ([quality])"
+const DEFAULT_TITLE_FORMAT = "[playlist_number] [title] ([quality])"
 #const DEFAULT_OUTPUT_FOLDER = "%USERPROFILE%/downloads"
 
 const DEFAULT_WINDOWS_SIZE = Vector2i(864, 140)
 const SETTINGS_WINDOWS_SIZE = Vector2i(864, 486)
 const ANIMATION_SPEED : float = 0.75
+const DEFAULT_OPACITY : float = 0.9
 
 @export var selectors : Array[SingleSelectContainer]
 
@@ -19,6 +20,9 @@ const ANIMATION_SPEED : float = 0.75
 @onready var audio: Button = %Audio
 @onready var video: Button = %Video
 @onready var mute: Button = %Mute
+@onready var background: Panel = %Background
+@onready var queue_completion: ProgressBar = %QueueCompletion
+@onready var download_completion: ProgressBar = %DownloadCompletion
 
 
 var first_press : bool = true
@@ -26,21 +30,41 @@ var using_audio : bool = false:
 	set(val):
 		mute.text = "🔊" if val else "🔈  "
 		using_audio = val
+		File.save_var("using_audio", using_audio)
 	
-var using_video : bool = false
+var using_video : bool = false:
+	set(val):
+		using_video = val
+		File.save_var("using_video", using_video)
+
 var settings_open : bool = false
 var output_path : String = ""
 var title_format : String = DEFAULT_TITLE_FORMAT
 var goin_up : bool 
 var disable_animations : bool = false 
 
+var settings : Dictionary[String, String]
+
 func _ready() -> void:
+	attempt_clipboard_link_paste()
 	load_settings()
 	get_window().size = SETTINGS_WINDOWS_SIZE
 	await get_tree().process_frame
 	get_window().size = DEFAULT_WINDOWS_SIZE
 
 func load_settings():
+	using_video = File.load_var("using_video", true)
+	video.button_pressed = using_video
+	
+	using_audio = File.load_var("using_audio", false)
+	if using_video:
+		audio.button_pressed = false 
+		_on_video_pressed(using_audio)
+		
+	else:
+		audio.button_pressed = using_audio
+		_on_audio_pressed()
+	
 	output_path = File.load_var("output_path", OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS))
 	output_folder.text = output_path
 	
@@ -49,19 +73,27 @@ func load_settings():
 	
 	disable_animations = File.load_var("disable_animations", false)
 	disable_animations_check.button_pressed = disable_animations
+	set_backgorund_alpha(1.0 if disable_animations else DEFAULT_OPACITY)
+	
 	
 	for selector : SingleSelectContainer in selectors:
-		selector.update_button_state(File.load_var(selector.update_id, selector.default_pressed_id))
+		var starting_setting : String = File.load_var(selector.update_id, selector.default_pressed_id)
+		selector.update_button_state(starting_setting)
+		settings[selector.update_id] = starting_setting
 
 
+func attempt_clipboard_link_paste():
+	var clipboard : String = DisplayServer.clipboard_get()
+	if Util.is_valid_url(clipboard, true):
+		link_entry.text = clipboard
 
-
-
+func set_backgorund_alpha(to : float):
+	background.get_theme_stylebox("panel").bg_color.a = to
 
 
 func _on_audio_pressed() -> void:
 	if video.button_pressed and audio.button_pressed and not first_press:
-		ui_animations.play("audio_back")
+		ui_animations.play("audio_back", -1, 1000.0 if disable_animations else 1.0)
 	
 	audio.button_pressed = true
 	video.button_pressed = false
@@ -74,16 +106,15 @@ func _on_audio_pressed() -> void:
 	mute.text = "🔊"
 
 
-
-func _on_video_pressed() -> void:
+func _on_video_pressed(set_audio : bool = true) -> void:
 	if video.button_pressed and audio.button_pressed or first_press:
-		ui_animations.play("audio")
+		ui_animations.play("audio", -1, 1000.0 if disable_animations else 1.0)
 	
 	audio.button_pressed = false
 	video.button_pressed = true
 	mute.button_pressed = false
 	
-	using_audio = true
+	using_audio = set_audio
 	using_video = true
 	first_press = false
 
@@ -112,11 +143,7 @@ func _on_settings_pressed() -> void:
 		
 		
 	else:
-		
-		
 		goin_up = get_window().position.y > DisplayServer.screen_get_size().y- SETTINGS_WINDOWS_SIZE.y
-		
-		
 		
 		if goin_up: tween.tween_property(get_window(), "position:y", get_window().position.y - SETTINGS_WINDOWS_SIZE.y + 140.0, ANIMATION_SPEED if not disable_animations else 0.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 		tween.tween_property(get_window(), "size", SETTINGS_WINDOWS_SIZE, ANIMATION_SPEED if not disable_animations else 0.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
@@ -135,7 +162,7 @@ func _on_settings_pressed() -> void:
 	#get_viewport().set_si
 
 
-func _on_output_folder_text_submitted(a = null) -> void:
+func _on_output_folder_text_submitted(_a = null) -> void:
 	var new_text : String = output_folder.text
 	if DirAccess.dir_exists_absolute(new_text) and new_text != "":
 		output_path = new_text
@@ -160,7 +187,7 @@ func _on_title_format_reset_pressed() -> void:
 	_on_title_format_text_submitted()
 
 
-func _on_title_format_text_submitted(a = null) -> void:
+func _on_title_format_text_submitted(_a = null) -> void:
 	var new_text : String = title_format_edit.text
 	if new_text.is_valid_filename():
 		title_format = new_text
@@ -171,12 +198,77 @@ func _on_title_format_text_submitted(a = null) -> void:
 		title_format_edit.text = title_format
 		print("Invalid title format: " + new_text)
 
-
 func _on_setting_selection_updated(active_button_id: String, update_id: String) -> void:
+	settings[update_id] = active_button_id
+	
 	File.save_var(update_id, active_button_id)
-	print("saved " + update_id + " to " + active_button_id)
+	#print("saved " + update_id + " to " + active_button_id)
 
 
 func _on_disable_animations_check_toggled(toggled_on: bool) -> void:
 	disable_animations = toggled_on
+	set_backgorund_alpha(1.0 if disable_animations else DEFAULT_OPACITY)
 	File.save_var("disable_animations", disable_animations)
+
+
+func _on_link_submitted(_a = null) -> void:
+	var link : String = link_entry.text
+	var hook : Dictionary = YTDLP.DEFAULT_PROGRESS_HOOK.duplicate()
+	
+	if using_video:
+		link_entry.text = ""
+		YTDLP.download_video(link, output_path, title_format, settings["video_format"], settings["video_quality"], not using_audio, hook)
+		update_progress_bars(hook)
+		
+	elif using_audio:
+		link_entry.text = ""
+		YTDLP.download_audio(link, output_path, title_format, settings["audio_format"], settings["audio_quality"], hook)
+		update_progress_bars(hook)
+	
+
+const DOWNLOAD_COMPLETION_SNAPPINESS = 2.0
+func update_progress_bars(hook : Dictionary):
+	download_completion.show()
+	download_completion.indeterminate = true
+	#var completion_tween : Tween = create_tween()
+	var prev_current = -1.0
+	ui_animations.play_backwards("fade_progress")
+	while not hook["done"]:
+		
+		#print("bbb")
+		#if hook["current"] == 0.0 and prev_current != 0.0:
+			#print("ccc")
+			#completion_tween.kill()
+			#download_completion.value = -0.2
+			#completion_tween = create_tween()
+			#completion_tween.tween_property(download_completion, "value", 0.0, YTDLP.APPROX_INIT_TIME)
+		
+		queue_completion.get_parent().visible = hook["playlist"] != -1
+		queue_completion.max_value = hook["playlist_total"] 
+		queue_completion.value = lerpf(queue_completion.value, float(hook["playlist"] - (1)), get_process_delta_time() * DOWNLOAD_COMPLETION_SNAPPINESS)
+		
+		if not hook["is_audio"]:
+			download_completion.value = hook["current"]
+		else:
+			if hook["current"] >= prev_current:
+				download_completion.value = lerpf(download_completion.value, hook["current"], get_process_delta_time() * DOWNLOAD_COMPLETION_SNAPPINESS)
+			else:
+				download_completion.value = hook["current"]
+				
+		if hook["current"] != 0.0:
+			download_completion.indeterminate = false
+		
+		prev_current = hook["current"]
+		await get_tree().process_frame
+		
+	Util.tween(download_completion, "value", download_completion.max_value, 1.0, Tween.EASE_OUT, Tween.TRANS_CUBIC)
+	Util.tween(queue_completion, "value", queue_completion.max_value, 1.0, Tween.EASE_OUT, Tween.TRANS_CUBIC)
+	
+	ui_animations.play("fade_progress", -1, 0.4)
+	await ui_animations.animation_finished
+	download_completion.value = 0.0
+	queue_completion.value = 0.0
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_WINDOW_FOCUS_IN:
+		attempt_clipboard_link_paste()
--- scripts/ytdlp_wrapper.gd
+++ scripts/ytdlp_wrapper.gd
@@ -1,77 +1,334 @@
 extends Node
 
-const FILE_NAME = "yt-dlp.exe"
-const ZIP_LOCATION : String = "res://resources/yt-dlp.zip"
-const LOCATION : String = "user://" + FILE_NAME
-
-enum VideoFormat {
-	ORIGINAL,
-	MP4,
-	MKV,
-	AVI,
-	WEBM
+enum Binary {
+	YTDLP,
+	FFMPEG,
+	FFPROBE
 }
 
-enum VideoQuality {
-	MAX,
-	_4K,
-	_1440P,
-	_1080P,
-	_720P,
-	_480P,
-	_240P,
-	_144P
+const ARG_BINDINGS : Dictionary[String, Array] = {
+	# audio format args
+	"mp3": ["--extract-audio", "--audio-format", "mp3"],
+	"wav" : ["--extract-audio", "--audio-format", "wav"],
+	"ogg" : ["--extract-audio", "--audio-format", "ogg"],
+	"flac" : ["--extract-audio", "--audio-format", "flac"],
+	"audio_original" : [],
+	
+	# audio quality args
+	"320kbps" : ["--audio-quality", "320K"],
+	"256kbps" : ["--audio-quality", "256K"],
+	"128kbps" : ["--audio-quality", "128K"],
+	"96kbps" : ["--audio-quality", "96K"],
+	"64kbps" : ["--audio-quality", "64K"],
+	"8kbps" : ["--audio-quality", "8K"],
+	
+	## video codec args
+	#"h264" : ["--codec", "video:h264,audio:aac"],
+	#"av1" : ["--codec", "video:av1,audio:aac"],
+	#"vp9" : ["--codec", "video:vp9,audio:opus"],
+	#"uncompressed" : [],
+	
+	# video format args
+	"mp4" : ["--recode-video", "mp4"],
+	"mkv" : ["--recode-video", "mkv"],
+	"avi" : ["--recode-video", "avi"],
+	"webm" : ["--recode-video", "webm"],
+	"video_original" : [],
+	
+	# video quality args
+	"max" : ["-f", "\"bestvideo+bestaudio/best\""],
+	"4k" : ["-f", "\"bestvideo[height<=2160]+bestaudio/best[height<=2160]/best\""],
+	"1440p" : ["-f","\"bestvideo[height<=1440]+bestaudio/best[height<=1440]/best\"" ],
+	"1080p" : ["-f", "\"bestvideo[height<=1080]+bestaudio/best[height<=1080]/best\""],
+	"720p" : ["-f", "\"bestvideo[height<=720]+bestaudio/best[height<=720]/best\""],
+	"480p" : ["-f", "\"bestvideo[height<=480]+bestaudio/best[height<=480]/best\""],
+	"360p" : ["-f", "\"bestvideo[height<=360]+bestaudio/best[height<=360]/best\""],
+	"240p" : ["-f", "\"bestvideo[height<=240]+bestaudio/best[height<=240]/best\""],
+	"144p" : ["-f", "\"bestvideo[height<=144]+bestaudio/best[height<=144]/best\""]
 }
 
-enum AudioFormat {
-	ORIGINAL,
-	MP3,
-	WAV,
-	OGG,
-	FLAC
+#const CONTAINER_BINDINGS : Dictionary[String, String] = {
+	#"mp4" : "h264",
+	#"avi" : 
+#}
+
+
+const BINARY_NAMES : Dictionary[Binary, String] = {
+	Binary.YTDLP : "yt-dlp.exe",
+	Binary.FFMPEG : "ffmpeg.exe",
+	Binary.FFPROBE : "ffprobe.exe" 
 }
 
-enum AudioQuality {
-	KBPS_320,
-	KBPS_256,
-	KBPS_128,
-	KBPS_96,
-	KBPS_64,
-	KBPS_8,
+const ZIP_NAMES : Dictionary[Binary, String] = {
+	Binary.YTDLP : "yt-dlp.zip",
+	Binary.FFMPEG : "ffmpeg.zip",
+	Binary.FFPROBE : "ffprobe.zip" 
 }
 
-var source_dir : String 
+const FILENAME_FORMAT_FLAGS : Dictionary[String, String] = {
+	"[playlist_number]" : "%(playlist_autonumber|)s",
+	"[title]" : "%(title)s",
+	"[author]" : "%(creator|Unknown)s",
+	#"[quality]" : "%(asr)s",
+	#"[quality]" : "%(height)sp"
+}
+
+const ZIPS_LOCATION : String = "res://bin/"
+const BINARY_LOCATION : String = "user://bin/"
+
+const FIND_DEFAULT_BROWSER_COMMAND : String = "powershell -command \"(Get-ItemProperty 'HKCU:\\Software\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\http\\UserChoice').ProgId\""
+const BROWSER_NORMALIZATOR9000 : Dictionary[String, String] = {
+	"ChromeHTML": "chrome",
+	"FirefoxURL": "firefox",
+	"MSEdgeHTM": "edge",
+	"BraveHTML": "brave",
+	"OperaStable": "opera",
+	"VivaldiHTM": "vivaldi",
+	"NaverWale": "whale",
+	"WhaleHTML": "whale",
+}
+
+var web_browser : String = ""
+
+const MAX_DOWNLOAD_TRANSCODE_RATIO_HISTORY : int = 8
+const APPROX_INIT_TIME = 4.0
+
+var download_transcode_ratio_history : Array[float] = [0.018]
+var download_transcode_ratio : float:
+	get():
+		var total : float = 0.0
+		for ratio : float in download_transcode_ratio_history:
+			total += ratio
+		
+		return total / download_transcode_ratio_history.size()
+
+var child_processes_ids : Array[int] = []
 
 func _ready() -> void:
-	source_dir = LOCATION
-	source_dir = source_dir.replace(FILE_NAME, "")
+	# find default web browser
+	var output = []
+	OS.execute("CMD.exe", ["/C", FIND_DEFAULT_BROWSER_COMMAND], output)
+	for browser_prog_id : String in BROWSER_NORMALIZATOR9000.keys():
+		if output[0].begins_with(browser_prog_id):
+			web_browser = BROWSER_NORMALIZATOR9000[browser_prog_id]
 	
-	if FileAccess.file_exists(LOCATION):
-		# update ytdlp
-		await update_ytdlp()
+	if web_browser == "": # uh oh!
+		printerr("Web browser could not be found: " + str(output[0]))
+		web_browser = "chromium"#? ?? ?!? 
 	
-	else:
-		File.extract_all_from_zip(ZIP_LOCATION, source_dir)
+	print("Default browser: " + web_browser)
+	
+	# make sure all the bianaries exist
+	File.verify_dir(BINARY_LOCATION)
+	for key in ZIP_NAMES.keys():
+		var zip_location : String = ZIPS_LOCATION + ZIP_NAMES[key]
+		var bin_location : String = BINARY_LOCATION + BINARY_NAMES[key]
 		
-		print("Created " + FILE_NAME + " at " + ProjectSettings.globalize_path(source_dir))
-		await update_ytdlp()
+		assert(FileAccess.file_exists(zip_location))
+		if not FileAccess.file_exists(bin_location):
+			File.extract_all_from_zip(zip_location, BINARY_LOCATION, false)
+			print("Created " + BINARY_NAMES[key] + " at " + ProjectSettings.globalize_path(bin_location))
+	
+	await update_ytdlp()
+	
+	# load save data
+	download_transcode_ratio_history = File.load_var("download_transcode_ratio_history", download_transcode_ratio_history)
 
-func run(args : Array[String], console : bool = false, print_output : bool = false):
-	var output : Array = []
+
+
+func run(args : Array[String], binary : Binary = Binary.YTDLP, console : bool = false, print_output : bool = false, block : bool = true, print_input : bool = false):
 	var concatinated_args : String = ""
 	for arg : String in args:
 		concatinated_args += " " + arg
 	
-	OS.execute("CMD.exe", ["/C", "cd " + ProjectSettings.globalize_path(source_dir) + " && " + FILE_NAME + concatinated_args], output, false, console)
-	if print_output:
-		print(str(output[0]))
+	if print_input:
+		print("RUNNING COMMAND:\ncd " + ProjectSettings.globalize_path(BINARY_LOCATION) + " && start \"\" /belownormal /b /wait " + BINARY_NAMES[binary] + concatinated_args)
+	
+	var output : Array = []
+	
+	if block : 
+		OS.execute("CMD.exe", ["/C", "cd " + ProjectSettings.globalize_path(BINARY_LOCATION) + " && start \"\" /belownormal /b /wait " + BINARY_NAMES[binary] + concatinated_args], output, false, console)
+		if print_output:
+			print(output[0])
+		
+	else:
+		var pipe : Dictionary = OS.execute_with_pipe("CMD.exe", ["/C", "cd " + ProjectSettings.globalize_path(BINARY_LOCATION) + " && start \"\" /belownormal /b /wait " + BINARY_NAMES[binary] + concatinated_args], false)
+		child_processes_ids.append(pipe["pid"])
+		return pipe
+	
+
+	
 
 func update_ytdlp():
 	print ("Verifying YT-DLP version...")
-	run(["-U"], false, true)
+	run(["--update"], Binary.YTDLP, false, true)
 
-func download_audio(link : String, output_dir : String, file_name: String, format : AudioFormat, quality : AudioQuality):
-	pass
+func format_filename(format_string : String):
+	var result : String = format_string
+	for key in FILENAME_FORMAT_FLAGS.keys():
+		result = result.replace(key, FILENAME_FORMAT_FLAGS[key])
+	
+	return result
 
-func download_video(link : String, output_dir : String, file_name: String, format : VideoFormat, quality : VideoQuality):
-	pass
+const SET_PROGRESS_IGNORE_AMMOUNT = [0, 60]
+var current_set_progress_timer : int = 1
+
+func _set_progress(to : float, progress_hook : Dictionary):
+	#current_set_progress_timer -= 1
+	#print("tuahweening")
+	#if current_set_progress_timer == 0:
+	#print("hawkweening")
+	progress_hook["current"] = to
+	current_set_progress_timer = Util.randi_array(SET_PROGRESS_IGNORE_AMMOUNT)
+
+func update_progress(process_hook : Dictionary, progress_hook : Dictionary, using_audio : bool):
+	var io : FileAccess = process_hook["stdio"]
+	var err : FileAccess = process_hook["stderr"]
+	var process_id : int = process_hook["pid"]
+	var is_playlist : bool = false
+	var time_start : float = Util.TIME
+	var download_time : float = 0.0
+	var transcode_time : float = 0.0
+	var transcode_flag : bool = false
+	var finished_flag : bool = false
+	
+	while OS.is_process_running(process_id):
+		await get_tree().process_frame
+		#if Util.run_every(10, self):
+			#print(progress_hook["current"])
+		
+		var output : String = io.get_as_text().remove_chars("\n\r")
+		var errors : String = err.get_as_text()
+		if output.length() > 3:
+			if output.contains("[download]"):
+				var last_percent : int = output.rfind("% of ")
+				#print("a " + str(last_percent))
+				if output.contains("d] Desti"):
+					time_start = Util.TIME
+					progress_hook["current"] = 0.0
+					
+				
+				if output.contains("100.0% of") and not finished_flag:
+					finished_flag = true
+					download_time = Util.TIME - time_start
+					#print("detected finish! ", download_time)
+					
+					if not using_audio:
+						var predicted_transcode_time : float = download_time / download_transcode_ratio
+						var tween : Tween = create_tween()
+						tween.tween_method(_set_progress.bind(progress_hook), progress_hook["current"], 1.0, predicted_transcode_time).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+						progress_hook["tween"] = tween
+					
+					else:
+						progress_hook["current"] = 1.0
+						
+					
+					
+				elif last_percent != -1 and not finished_flag:
+					#print("b!!")
+					var split : PackedStringArray = output.split(" ")
+					#print(split)
+					for i : int in range(0, split.size() - 1):
+						#print("split: ", split[i], " and ", split[i+1])
+						if split[i].contains("%") and split[i+1] == "of":
+							progress_hook["current"] = float(split[i].remove_chars("%")) / 100.0 * (download_transcode_ratio if not using_audio else 1.0)
+							#print("c!!", progress_hook["current"])
+							break
+					
+					
+					
+				elif output.find("] Playlist "):
+					is_playlist = not output.contains(" items of 1")
+				
+				if is_playlist and output.contains("] Downloading item "):
+					var split : PackedStringArray = output.split(" ")
+					for i : int in range(split.size()):
+						if split[i] == "Downloading" and split[i+1] == "item" and split[i+3] == "of":
+							progress_hook["playlist_total"] = int(split[i+4])
+							progress_hook["playlist"] = int(split[i+2])
+							finished_flag = false
+							progress_hook["current"] = 0.0
+							break
+			
+			if not using_audio:
+				if not transcode_flag and output.contains("[VideoConvertor] Converting video from"):
+					transcode_flag = true
+				
+				elif transcode_flag and output.contains("Deleting original file"):
+					transcode_flag = false
+					
+					transcode_time = Util.TIME - time_start - download_time
+					#print("download_time ", download_time, " transcode_time ", transcode_time)
+					if progress_hook.has("tween") and progress_hook["tween"]:
+						progress_hook["tween"].kill()
+					
+					var ratio : float = download_time / transcode_time
+					if ratio < 0.1:
+						download_transcode_ratio_history.append(ratio)
+					
+					if download_transcode_ratio_history.size() > MAX_DOWNLOAD_TRANSCODE_RATIO_HISTORY:
+						download_transcode_ratio_history.pop_front()
+					
+					File.save_var("download_transcode_ratio_history", download_transcode_ratio_history)
+					
+				
+				
+			
+			print(output)
+		
+		if errors.length() > 3:
+			printerr(errors) # TODO add on screen error printer
+	
+	progress_hook["done"] = true
+const DEFAULT_PROGRESS_HOOK : Dictionary = {"current": 0.0, "playlist": -1, "playlist_total" : -1, "is_audio" : false, "done" : false}
+
+
+func download_audio(link : String, output_dir : String, file_name_format: String, format : String, quality : String, progress_hook : Dictionary = DEFAULT_PROGRESS_HOOK.duplicate(), is_playlist : bool = false):
+	var args : Array[String] = [
+		link,
+		"-o", "\"" + format_filename(file_name_format).replace("[quality]", quality) + ".%(ext)s" + "\"",
+		"-P", "\"" + output_dir + "\"",
+		"--cookies-from-browser", web_browser,
+		"--embed-metadata"
+		#"--yes-playlist" if is_playlist else "--no-playlist",
+		#"--newline",
+		
+	]
+	
+	args.append_array(ARG_BINDINGS[format])
+	args.append_array(ARG_BINDINGS[quality])
+	
+	var runtime_info : Dictionary = run(args, Binary.YTDLP, false, false, false, true)
+	progress_hook["is_audio"] = true
+	update_progress(runtime_info, progress_hook, true)
+	return progress_hook
+
+func download_video(link : String, output_dir : String, file_name_format: String, format : String, quality : String, muted : bool = false, progress_hook : Dictionary = DEFAULT_PROGRESS_HOOK.duplicate(), is_playlist : bool = false):
+	var args : Array[String] = [
+		link,
+		"-o", "\"" + format_filename(file_name_format).replace("[quality]", "%(height)sp") + ".%(ext)s" + "\"",
+		"-P", "\"" + output_dir + "\"",
+		"--cookies-from-browser", web_browser,
+		"--embed-metadata"
+		#"--yes-playlist" if is_playlist else "--no-playlist",
+		#"--newline",
+	]
+	
+	if muted: args.append("--mute")
+	
+	#args.append_array(ARG_BINDINGS[codec])
+	args.append_array(ARG_BINDINGS[format])
+	args.append_array(ARG_BINDINGS[quality])
+	
+	var runtime_info : Dictionary = run(args, Binary.YTDLP, false, false, false, true)
+	update_progress(runtime_info, progress_hook, false)
+	return progress_hook
+
+func KILL_ALL_CHILDREN(): # MWHAHAHAHAHAHAHHA!!!
+	for pid : int in child_processes_ids:
+		OS.kill(pid)
+
+func  _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		KILL_ALL_CHILDREN()
+		get_tree().quit()
